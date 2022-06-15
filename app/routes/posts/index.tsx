e7ad229b9c0f1311e5668656045245a851743a3e
Tam Le <tamsgit@gmail.com>
2022-06-15 12:44:05 -0400

    Render loaded data in Posts' view logic

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,5 +1,5 @@
 import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 
 export const loader = async () => {
   return json({
@@ -18,10 +18,21 @@ export const loader = async () => {
 
 export default function Posts() {
   const { posts } = useLoaderData();
-  console.log(posts);
   return (
     <main>
       <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link
+              to={post.slug}
+              className="text-blue-600 underline"
+            >
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
